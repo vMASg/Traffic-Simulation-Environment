57253d508c242098823d10d0eead48a2e0e7ef72
Víctor Mas <victor.mas@est.fib.upc.edu>
2016-06-11 16:01:49 +0200

    changed tabs -> spaces in pipeline_executor

--- server/resources/pipeline_executor.py
+++ server/resources/pipeline_executor.py
@@ -9,15 +9,15 @@ class PipelineExecutor(object):
         self.script_service = script_service
 
     def run_pipeline(self, id):
-    	pipeline_path = self.pipeline_service.get_path_for_execution(id)
-    	with open(pipeline_path, 'r') as f:
-    		pipeline = json.loads(f.read())
+        pipeline_path = self.pipeline_service.get_path_for_execution(id)
+        with open(pipeline_path, 'r') as f:
+            pipeline = json.loads(f.read())
 
-    	# Change path for all scripts
-    	for script in pipeline:
-    		script['path'] = self.script_service.get_path_for_execution(script['path'])
+        # Change path for all scripts
+        for script in pipeline:
+            script['path'] = self.script_service.get_path_for_execution(script['path'])
 
-    	with open(pipeline_path, 'w') as f:
-    		f.write(json.dumps(pipeline))
+        with open(pipeline_path, 'w') as f:
+            f.write(json.dumps(pipeline))
 
         return self.aimsun_service.run_pipeline(pipeline_path)

87a2360c00bfd8345447e19d5dd7ba6194a1c7b6
Víctor Mas <victor.mas@est.fib.upc.edu>
2016-09-06 20:27:56 +0200

    Adding panel header

---
+++ client/app/js/directives/panelheader.js
@@ -0,0 +1,10 @@
+angular.module('trafficEnv')
+    .directive('panelHeader', function(){
+        return {
+            templateUrl: 'templates/panel-header.html',
+            controller: ['$scope',
+            function($scope) {
+
+            }],
+        };
+    });

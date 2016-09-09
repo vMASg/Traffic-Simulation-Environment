8e54266f02cae6ab66fe3c57a9cdaefa2ba080cf
Víctor Mas <victor.mas@est.fib.upc.edu>
2016-09-09 12:40:38 +0200

    Fixed dynamic compilation of controller

--- client/app/js/directives/interfacetab.js
+++ client/app/js/directives/interfacetab.js
@@ -1,10 +1,32 @@
 angular.module('trafficEnv')
     .directive('interfaceTab', ['$compile', '$timeout', function($compile, $timeout) {
         var self = this;
+        var queueLen = angular.module('trafficEnv')._invokeQueue.length;
+
         function addScript (path) {
-            self.s = document.createElement('script');
-            s.src = path;
-            document.body.appendChild(s);
+            var scriptElement = document.createElement('script');
+            scriptElement.src = path;
+            self.s = angular.element(scriptElement);
+            angular.element(document.body).append(self.s);
+        }
+
+        function registerRecompile(element, $scope) {
+            var queue = angular.module('trafficEnv')._invokeQueue;
+            for(var i=queueLen;i<queue.length;i++) {
+                var call = queue[i];
+                // call is in the form [providerName, providerFunc, providerArguments]
+                var provider = providers[call[0]];
+                if(provider) {
+                    // e.g. $controllerProvider.register("Ctrl", function() { ... })
+                    provider[call[1]].apply(provider, call[2]);
+                }
+            }
+
+            // compile the new element
+            element.injector().invoke(function() {
+                $compile(element)($scope);
+                $scope.$apply();
+            });
         }
 
         return {
@@ -19,9 +41,10 @@ angular.module('trafficEnv')
 
                     // var directive = directiveGetter(scope);
                     if (!element.attr('ng-include')) {
-                        element.attr('ng-include', '"' + 'interfaces/' + encodeURIComponent($scope.data.id.replace(/\.intf$/, '.html')) + '"');
                         addScript('interfaces/' + encodeURIComponent($scope.data.id.replace(/\.intf$/, '.js')));
-                        $timeout(function() { $compile(element)($scope); }, 5000);
+                        element.attr('ng-include', '"' + 'interfaces/' + encodeURIComponent($scope.data.id.replace(/\.intf$/, '.html')) + '"');
+                        // console.log(angular.module('trafficEnv')._invokeQueue.map(a => a[2][0]));
+                        $timeout(function () { registerRecompile(element, $scope); }, 2000);
                         $scope.$on('$destroy', function () {
                             document.body.removeChild(self.s);
                         });
--- client/app/js/main.js
+++ client/app/js/main.js
@@ -1,4 +1,10 @@
-angular.module('trafficEnv', ['treeControl', 'ui.ace', 'APIServices', 'ui.bootstrap', 'btford.socket-io', 'ang-drag-drop'])
+angular.module('trafficEnv', ['treeControl', 'ui.ace', 'APIServices', 'ui.bootstrap', 'btford.socket-io', 'ang-drag-drop'], function($controllerProvider, $compileProvider, $provide) {
+    providers = {
+        $controllerProvider: $controllerProvider,
+        $compileProvider: $compileProvider,
+        $provide: $provide
+    };
+})
 
     .factory('socket', ['socketFactory', function(socketFactory){
         return socketFactory({
